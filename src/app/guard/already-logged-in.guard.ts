4f443f91d246ea47781bd55010af1bb5b276da9b
dborovac <borovacdario4@gmail.com>
2023-03-11 20:54:32 +0100

    uvezan login sa backend-om, template search employees komponente

--- src/app/guard/already-logged-in.guard.ts
+++ src/app/guard/already-logged-in.guard.ts
@@ -5,14 +5,14 @@ import { AuthService } from '../service/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AlreadyLoggedInGuard implements CanActivate {
   constructor(private router: Router,
               private authService: AuthService){};
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-        if (!this.authService.isLoggedIn()) {
-            this.router.navigate(['/login']);
+        if (this.authService.isLoggedIn()) {
+            this.router.navigate(['/']);
             return false;
         }
         return true;
