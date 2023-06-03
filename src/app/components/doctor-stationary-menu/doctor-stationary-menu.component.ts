1c1c6b316dadace981b6e764b78f1e54440408e2
dborovac <borovacdario4@gmail.com>
2023-06-03 19:40:34 +0200

    lbp

--- src/app/components/doctor-stationary-menu/doctor-stationary-menu.component.ts
+++ src/app/components/doctor-stationary-menu/doctor-stationary-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-stationary-menu',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctor-stationary-menu.component.css']
 })
 export class DoctorStationaryMenuComponent implements OnInit {
+  lbp: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.lbp = params.lbp;
+    })
+  }
 
   ngOnInit(): void {
   }
